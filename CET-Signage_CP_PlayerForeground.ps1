a09c7527bff979d3f42e4f7fa61c283f7a3350da
CET Signage Operations <CETSIGN@microsoft.com>
2025-11-05 10:25:26 -0800

    Add files via upload

---
+++ CET-Signage_CP_PlayerForeground.ps1
@@ -0,0 +1,13 @@
+﻿$sig = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+Add-Type -MemberDefinition $sig -name NativeMethods -namespace Win32
+add-type -AssemblyName microsoft.VisualBasic
+add-type -AssemblyName System.Windows.Forms
+
+$CP = @(Get-Process "signage")[0].MainWindowHandle
+
+[Win32.NativeMethods]::ShowWindowAsync($CP, 11)
+[Win32.NativeMethods]::ShowWindowAsync($CP, 3)
+
+Start-Sleep -Seconds 0.5
+
+[Microsoft.VisualBasic.Interaction]::AppActivate("Content Player")
