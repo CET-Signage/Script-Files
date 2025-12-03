fc66e32ac18165056ed4e0ae9cc31cd1f7692016
CET Signage Operations <CETSIGN@microsoft.com>
2025-12-03 12:56:53 -0800

    Add files via upload

---
+++ CET-Signage_Device-Script_Player-Minimize.ps1
@@ -0,0 +1,11 @@
+﻿# This script minimizes Content Player
+
+# Load dll and define method
+$sig = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+Add-Type -MemberDefinition $sig -name NativeMethods -namespace Win32
+
+# Find the process ID for the content player
+$hwnd = @(Get-Process "signage")[0].MainWindowHandle
+
+# Minimize Content Player
+[Win32.NativeMethods]::ShowWindowAsync($hwnd, 11)
